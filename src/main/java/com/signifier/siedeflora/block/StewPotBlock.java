dd6c1abfa7ff0d73b8a8da3e9ba068f1ae4a64cd
Loci <l_natit@outlook.com>
2026-04-16 12:23:38 +0800

    Add two blocks, WIP

---
+++ src/main/java/com/signifier/siedeflora/block/StewPotBlock.java
@@ -0,0 +1,9 @@
+package com.signifier.siedeflora.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class StewPotBlock extends Block {
+    public StewPotBlock(Properties properties) {
+        super(properties);
+    }
+}
---
+++ src/main/java/com/signifier/siedeflora/block/StoveBlock.java
@@ -0,0 +1,9 @@
+package com.signifier.siedeflora.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class StoveBlock extends Block {
+    public StoveBlock(Properties properties) {
+        super(properties);
+    }
+}
